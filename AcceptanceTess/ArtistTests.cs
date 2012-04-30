a7194c064f97dbb1f7c071aa13e15cf002ea6bd7
Nick <nick@ntcoding.co.uk>
2012-04-30 14:57:08 +0100

    Failing AAT for artist / get

--- AcceptanceTess/ArtistTests.cs
+++ AcceptanceTess/ArtistTests.cs
@@ -9,8 +9,8 @@ namespace AcceptanceTess
         [Test]
         public void Responds_with_404_when_artist_does_not_exist()
         {
-            Requester.ExamineResponseFor("artist/doesnotexist", r => Assert.That(r.StatusCode, Is.EqualTo(HttpStatusCode.NotFound)));
+            Requester.ExamineResponseFor("artist/doesnotexist", 
+                r => Assert.That(r.StatusCode, Is.EqualTo(HttpStatusCode.NotFound)));
         }
-
     }
 }
---
+++ FubuRESTInnovation/Handlers/Artist/Get.cs
@@ -0,0 +1,23 @@
+﻿using FubuMVC.Core;
+
+namespace FubuRESTInnovation.Handlers.Artist
+{
+    
+    public class Get
+    {
+        public ArtistResource Invoke(ArtistRequest input)
+        {
+            return new ArtistResource();
+        }
+    }
+
+    public class ArtistRequest
+    {
+        [RouteInput]
+        public string Key { get; set; }
+    }
+
+    public class ArtistResource
+    {
+    }
+}
