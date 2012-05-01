641a3dc99104ee3a4c02ba7d0724407011757962
Nick <nick@ntcoding.co.uk>
2012-05-01 14:36:36 +0100

    correct error message shown when parameter is invalid

--- FubuRESTInnovation/Infrastructure/Errors/BindingErrorsHandler.cs
+++ FubuRESTInnovation/Infrastructure/Errors/BindingErrorsHandler.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Net;
+using System.Web;
 using FubuMVC.Core.Behaviors;
 using FubuMVC.Core.Http;
 using FubuMVC.Core.Runtime;
@@ -12,11 +13,15 @@ namespace FubuRESTInnovation.Infrastructure.Errors
     {
         private readonly IFubuRequest _request;
         private readonly IHttpWriter _writer;
+        private readonly IRequestHeaders _headers;
+        private readonly HttpContextBase _context; // Low level access to the current context handled by IoC
 
-        public BindingErrorsHandler(IFubuRequest request, IHttpWriter writer)
+        public BindingErrorsHandler(IFubuRequest request, IHttpWriter writer, IRequestHeaders headers, HttpContextBase context)
         {
             _request = request;
             _writer = writer;
+            _headers = headers;
+            _context = context;
         }
 
         public IActionBehavior InsideBehavior { get; set; }
@@ -32,6 +37,8 @@ namespace FubuRESTInnovation.Infrastructure.Errors
             var enumError = bindingErrors.FirstOrDefault(b => b.Property.PropertyType.IsEnum);
             if (enumError != null)
             {
+                _context.Response.Clear();
+                ResponseContentSetter.SetErrorResponse(enumError.Value.RawValue + " is not a valid " + enumError.Property.Name.ToLower(), _writer, _headers);
                 _writer.WriteResponseCode(HttpStatusCode.BadRequest);
             }
 
